3f4af0564f73463f7c4a5224892fa7a17bd7be96
Jason Colburne <j.colburne@gmail.com>
2022-07-09 17:58:00 -0300

    some models/migrations

--- node-common/database/models.py
+++ node-common/database/models.py
@@ -1,8 +1,28 @@
+# Make sure you are editing this file in node-common
+
+from datetime import date
 from typing import Optional
 
 from sqlmodel import Field, SQLModel
 
 
 class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True, nullable=False)
     email: str
+
+class Role(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True, nullable=False)
+    name: str
+
+class UserRole(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True, nullable=False)
+    user_id: int = Field(foreign_key="user.id")
+    role_id: int = Field(foreign_key="role.id")
+
+class Article(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True, nullable=False)
+    digital_object_identifier: str
+    author: str
+    title: str
+    journal: str
+    year: date
