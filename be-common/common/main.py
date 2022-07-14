b1e0414ad3a71f0a5a88bc5106eda2d0b49cd956
Jason Colburne <j.colburne@gmail.com>
2022-07-14 04:59:28 -0300

    cleanup

--- be-common/common/main.py
+++ be-common/common/main.py
@@ -1,9 +1,12 @@
+import os
 import toml
 import uvicorn
 
+
+ENVIRONMENT = os.environ.get("DEPLOYMENT_ENVIRONMENT")
 
 def start():
     """Launched with `poetry run start` at root level"""
     project = toml.load("pyproject.toml")
     module = project["tool"]["poetry"]["name"]
-    uvicorn.run(f"{module}.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(f"{module}.app:app", host="0.0.0.0", port=80, reload=(ENVIRONMENT == "development"))
