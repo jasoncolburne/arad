07a97e6bc7de10be497ac2e424956e3b6eaaf143
Jason Colburne <j.colburne@gmail.com>
2022-07-14 07:01:38 -0300

    create concept of environment

--- be-common/common/main.py
+++ be-common/common/main.py
@@ -3,11 +3,11 @@ import toml
 import uvicorn
 
 
-ENVIRONMENT = os.environ.get("DEPLOYMENT_ENVIRONMENT")
+DEPLOYMENT_ENVIRONMENT = os.environ.get("DEPLOYMENT_ENVIRONMENT")
 
 def start():
     """Launched with `poetry run start` at root level"""
     project = toml.load("pyproject.toml")
     module = project["tool"]["poetry"]["name"]
-    local = ENVIRONMENT == "development"
+    local = DEPLOYMENT_ENVIRONMENT == "development"
     uvicorn.run(f"{module}.app:app", host="0.0.0.0", port=(8000 if local else 80), reload=local)
