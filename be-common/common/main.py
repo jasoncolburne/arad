b14d6529cd1a1a908b9c4320b490d61519b46309
Jason Colburne <j.colburne@gmail.com>
2022-07-14 05:06:25 -0300

    make port conditional

--- be-common/common/main.py
+++ be-common/common/main.py
@@ -9,4 +9,5 @@ def start():
     """Launched with `poetry run start` at root level"""
     project = toml.load("pyproject.toml")
     module = project["tool"]["poetry"]["name"]
-    uvicorn.run(f"{module}.app:app", host="0.0.0.0", port=80, reload=(ENVIRONMENT == "development"))
+    local = ENVIRONMENT == "development"
+    uvicorn.run(f"{module}.app:app", host="0.0.0.0", port=(8000 if local else 80), reload=local)
