6e98c0e949c13ce4d4a356fb152fdfd060102862
Jason Colburne <j.colburne@gmail.com>
2022-07-22 08:42:31 -0300

    configurable ip/port for backend

--- be-common/common/main.py
+++ be-common/common/main.py
@@ -6,6 +6,8 @@ import logging
 
 
 DEPLOYMENT_ENVIRONMENT = os.environ.get("DEPLOYMENT_ENVIRONMENT")
+LISTEN_IP = os.environ.get("LISTEN_IP", "0.0.0.0")
+LISTEN_PORT = int(os.environ.get("LISTEN_PORT", "80"))
 LOCAL = DEPLOYMENT_ENVIRONMENT == "development"
 
 
@@ -13,4 +15,5 @@ def start():
     """Launched with `poetry run start` at root level"""
     project = toml.load("pyproject.toml")
     module = project["tool"]["poetry"]["name"]
-    uvicorn.run(f"{module}.app:app", host="0.0.0.0", port=80, reload=LOCAL)
+    entrypoint = f"{module}.app:app"
+    uvicorn.run(entrypoint, host=LISTEN_IP, port=LISTEN_PORT, reload=LOCAL)
