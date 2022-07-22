34886adc12e82a32b2d2add69bcc96ba76a4153d
Jason Colburne <j.colburne@gmail.com>
2022-07-22 04:05:32 -0300

    basic repl

--- be-common/common/repl.py
+++ be-common/common/repl.py
@@ -1,12 +1,6 @@
-from fastapi import FastAPI
+import sys
+sys.path.append('/app/')
 
+from database.models import *
 
-app = FastAPI()
-
-@app.middleware("errors")
-async def handle_errors(request: Request, call_next):
-    try:
-        return await call_next(request)
-    except (ex):
-        log.error(request, ex)
-        raise HttpException(code=500, message="Internal server error")
+%autoreload 3
