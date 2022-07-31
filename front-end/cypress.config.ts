ae9e790e1f046cc3540a2cc4300c287596ee5b86
Jason Colburne <j.colburne@gmail.com>
2022-07-31 16:41:46 -0300

    fix up local

--- front-end/cypress.config.ts
+++ front-end/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://front-end:3000",
+    baseUrl: "http://front-end",
     specPattern: ["**/*.cy.ts"],
   },
 });
