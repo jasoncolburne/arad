bcc180f75a06c96990ae0e10a6a32fb1c673a6bd
Jason Colburne <j.colburne@gmail.com>
2022-08-01 09:19:01 -0300

    clean up tooling (#135)

--- front-end/cypress/support/e2e.ts
+++ front-end/cypress/support/e2e.ts
@@ -1,6 +1,3 @@
 beforeEach("visit homepage", () => {
-  // documentation implies this isn't necessary but I needed this explicit call
-  // I assume these hooks get fired before the local storage is cleared by cypress
-  cy.clearLocalStorage();
   cy.visit("/").get("#arad-codeLink").should("be.visible");
 });
