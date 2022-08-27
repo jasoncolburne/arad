3efd7ebce3cf25d975c2ffa1963bc33c21c89a63
Jason Colburne <j.colburne@gmail.com>
2022-08-27 17:03:36 -0300

    remove unused unused-wildcard-import ignore

--- identity/identity/repl.py
+++ identity/identity/repl.py
@@ -2,7 +2,7 @@ import sys
 
 sys.path.append("/app/")
 
-from common.repl import *  # pylint: disable=unused-wildcard-import,wildcard-import,wrong-import-position
+from common.repl import *  # pylint: disable=wildcard-import,wrong-import-position
 import identity.orchestrations  # pylint: disable=unused-import,wrong-import-position
 
 db = database.get_session()
