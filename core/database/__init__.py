c8386e5c89f4e92c3d5b79a789f685014fc09b5c
Jason Colburne <j.colburne@gmail.com>
2022-08-28 08:39:21 -0300

    update core database file

--- core/database/__init__.py
+++ core/database/__init__.py
@@ -1,9 +1,7 @@
-# Make sure you are editing this file in arad/core
-
 import os
 
-from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine, create_async_engine
-from sqlalchemy.orm import sessionmaker
+import sqlalchemy.ext.asyncio
+import sqlalchemy.orm
 
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
@@ -11,11 +9,11 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 
 class DatabaseManager:
     def __init__(self) -> None:
-        self.engine: AsyncEngine | None = None
+        self.engine: sqlalchemy.ext.asyncio.AsyncEngine | None = None
 
-    def get_engine(self) -> AsyncEngine:
+    def get_engine(self) -> sqlalchemy.ext.asyncio.AsyncEngine:
         if self.engine is None:
-            self.engine = create_async_engine(
+            self.engine = sqlalchemy.ext.asyncio.create_async_engine(
                 DATABASE_URL,
                 pool_recycle=900,
                 pool_pre_ping=True,
@@ -28,10 +26,10 @@ class DatabaseManager:
 global_database_manager = DatabaseManager()
 
 
-async def get_session() -> AsyncSession:  # type: ignore
-    async_session = sessionmaker(
+async def get_session():  # type: ignore
+    async_session = sqlalchemy.orm.sessionmaker(
         global_database_manager.get_engine(),
-        class_=AsyncSession,
+        class_=sqlalchemy.ext.asyncio.AsyncSession,
         expire_on_commit=False,
     )
     async with async_session() as session:
