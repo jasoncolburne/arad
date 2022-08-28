110f2a3f3b478f76a121cbd67ab53ab0bf41279c
Jason Colburne <j.colburne@gmail.com>
2022-08-28 15:09:52 -0300

    better logging

--- core/common/main.py
+++ core/common/main.py
@@ -1,5 +1,6 @@
 # Make sure you are editing this file in arad/core
 
+import logging
 import os
 import toml
 import uvicorn
@@ -9,6 +10,46 @@ DEPLOYMENT_ENVIRONMENT = os.environ.get("DEPLOYMENT_ENVIRONMENT", "production")
 LISTEN_IP = os.environ.get("LISTEN_IP", "127.0.0.1")
 LISTEN_PORT = int(os.environ.get("LISTEN_PORT", "80"))
 LOCAL = DEPLOYMENT_ENVIRONMENT == "development"
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "format": "%(asctime)s %(levelprefix)-9s [%(name)s] %(message)s",
+        },
+        "access": {
+            "()": "uvicorn.logging.AccessFormatter",
+            "format": '%(asctime)s %(levelprefix)-9s [%(name)s] %(client_addr)s "%(request_line)s" %(status_code)s',
+        },
+    },
+    "handlers": {
+        "default": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+            "stream": "ext://sys.stdout",
+        },
+        "access": {
+            "class": "logging.StreamHandler",
+            "formatter": "access",
+            "stream": "ext://sys.stdout",
+        },
+    },
+    "loggers": {
+        "uvicorn": {
+            "level": "INFO",
+            "handlers": ["default"],
+        },
+        "uvicorn.error": {
+            "level": "INFO",
+        },
+        "uvicorn.access": {
+            "level": "INFO",
+            "propagate": False,
+            "handlers": ["access"],
+        },
+    },
+}
 
 
 def start() -> None:
@@ -16,4 +57,10 @@ def start() -> None:
     project = toml.load("pyproject.toml")
     module = project["tool"]["poetry"]["name"]
     entrypoint = f"{module}.app:app"
-    uvicorn.run(entrypoint, host=LISTEN_IP, port=LISTEN_PORT, reload=LOCAL)
+    uvicorn.run(
+        entrypoint,
+        host=LISTEN_IP,
+        port=LISTEN_PORT,
+        reload=LOCAL,
+        log_config=LOGGING_CONFIG,
+    )
