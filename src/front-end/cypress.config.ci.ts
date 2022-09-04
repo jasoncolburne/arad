c36d8e154c32e132ff6dd3de898c01fe2b2e5d4f
Jason Colburne <j.colburne@gmail.com>
2022-09-04 10:31:24 -0300

    turn on screenshotting for debug

--- src/front-end/cypress.config.ci.ts
+++ src/front-end/cypress.config.ci.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://front-end-nginx",
     specPattern: ["**/*.cy.ts"],
-    video: false,
-    screenshotOnRunFailure: false,
+    video: true,
+    screenshotOnRunFailure: true,
   },
 });
